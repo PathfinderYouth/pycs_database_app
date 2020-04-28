4c5366f3f7d00f7c0a68700b3232e9e4ea9031c2
EmilyDeLisle <emily.delisle@gmail.com>
2020-04-28 14:51:11 -0700

    Added  dependency. Started creating side nav drawer

--- src/database/components/Database.js
+++ src/database/components/Database.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import { Link } from '@reach/router';
+import { NavDrawer } from './NavDrawer';
 
 // container that holds all database UI objects
 export const Database = () => (
     <>
         <Typography variant='h3' >Database UI</Typography>
+        <NavDrawer/>
         {/* Components go here */}
         <Link to='/'><Typography>Back to the intake form</Typography></Link>
     </>
