7477fc239893283dc736f631309602e8eedd7a2b
Robert <robertvu483@gmail.com>
2020-04-30 06:05:01 -0700

    Restructured and added approve and get lists operations.

--- src/firebaseFacade.js
+++ src/firebaseFacade.js
@@ -16,6 +16,12 @@ const CONFIG = {
 const FieldValue = firebase.firestore.FieldValue;
 const Timestamp = firebase.firestore.Timestamp;
 
+const STATUS = {
+  approved: "Approved",
+  pending: "Pending",
+  deleted: "Deleted"
+}
+
 // TODO: converter, query, order, limit, paging, indices, cached
 
 export default class FirebaseFacade {
@@ -45,7 +51,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Add a new participant document into pending collection
+   * Add a new participant document into pending collection.
    * @param {participant: object}
    *  Object containing participant info
    * @param {onSuccess?: (docId: string) => void}
@@ -55,7 +61,7 @@ export default class FirebaseFacade {
    */
   addPending(participant, onSuccess, onError) {
     let document = Object.assign({}, participant, {
-      status: "Pending",
+      status: STATUS.pending,
       history: FieldValue.arrayUnion({
         user: "System",
         event: "Received registration data from paticipant.",
@@ -78,7 +84,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Get a participant document from pending collection
+   * Get a participant document from pending collection.
    * @param {docId: string}
    *  Document id
    * @param {onNext: (doc: Document) => void}
@@ -110,34 +116,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Get all participant documents from pending collection
-   * @param {onChildNext: (doc: Document, newIndex: number,
-   *                       oldIndex: number, type: string) => void}
-   *  Callback function when document changes in the collection
-   * @param {onError?: (error: Error) => void}
-   *  Callback function when fail
-   * @returns {() => void}
-   *  Unsubscribe function
-   */
-  getPendingList(onChildNext, onError) {
-    return this.pendingRef
-      .onSnapshot({
-        next: (querySnap) => {
-          querySnap.docChanges().forEach((docChg) => {
-            let doc = docChg.doc.data();
-            onChildNext(doc, docChg.newIndex, docChg.oldIndex, docChg.type);
-          });
-        },
-        error: (error) => {
-          if (onError) {
-            onError(error);
-          }
-        }
-      });
-  }
-  
-  /**
-   * Update a participant document in pending collection
+   * Update a participant document in pending collection.
    * @param {docId: string}
    *  Document id
    * @param {data: object}
@@ -151,7 +130,7 @@ export default class FirebaseFacade {
     let document = Object.assign({}, data, {
       history: FieldValue.arrayUnion({
         user: "TODO",
-        event: "TODO",
+        event: "TODO: Updated",
         timestamp: Timestamp.now()
       })
     });
@@ -170,7 +149,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Delete a participant document from pending collection
+   * Delete a participant document from pending collection.
    * @param {docId: string}
    *  Document id
    * @param {onSuccess?: () => void}
@@ -193,7 +172,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Add a new participant document into permanent collection
+   * Add a new participant document into permanent collection.
    * @param {participant: object}
    *  Object containing participant info
    * @param {onSuccess?: (docId: string) => void}
@@ -203,7 +182,7 @@ export default class FirebaseFacade {
    */
   addPermanent(participant, onSuccess, onError) {
     let document = Object.assign({}, participant, {
-      status: "Approved",
+      status: STATUS.approved,
       history: FieldValue.arrayUnion({
         user: "TODO",
         event: "TODO: Created",
@@ -226,7 +205,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Get a participant document from permanent collection
+   * Get a participant document from permanent collection.
    * @param {docId: string}
    *  Document id
    * @param {onNext: (doc: Document) => void}
@@ -258,34 +237,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Get all participant documents from permanent collection
-   * @param {onChildNext: (doc: Document, newIndex: number,
-   *                       oldIndex: number, type: string) => void}
-   *  Callback function when document changes in the collection
-   * @param {onError?: (error: Error) => void}
-   *  Callback function when fail
-   * @returns {() => void}
-   *  Unsubscribe function
-   */
-  getPermanentList(onChildNext, onError) {
-    return this.permanentRef
-      .onSnapshot({
-        next: (querySnap) => {
-          querySnap.docChanges().forEach((docChg) => {
-            let doc = docChg.doc.data();
-            onChildNext(doc, docChg.newIndex, docChg.oldIndex, docChg.type);
-          });
-        },
-        error: (error) => {
-          if (onError) {
-            onError(error);
-          }
-        }
-      });
-  }
-  
-  /**
-   * Update a participant document in permanent collection
+   * Update a participant document in permanent collection.
    * @param {docId: string}
    *  Document id
    * @param {data: object}
@@ -318,7 +270,7 @@ export default class FirebaseFacade {
   }
   
   /**
-   * Delete a participant document from permanent collection
+   * Delete a participant document from permanent collection.
    * @param {docId: string}
    *  Document id
    * @param {onSuccess?: () => void}
@@ -328,7 +280,7 @@ export default class FirebaseFacade {
    */
   deletePermanent(docId, onSuccess, onError) {
     let document = {
-      status: "Deleted",
+      status: STATUS.deleted,
       history: FieldValue.arrayUnion({
         user: "TODO",
         event: "TODO: Deleted",
@@ -347,5 +299,183 @@ export default class FirebaseFacade {
           onError(error);
         }
       });
+  }
+  
+  /**
+   * Undo deleting a participant document from permanent collection.
+   * @param {docId: string}
+   *  Document id
+   * @param {onSuccess?: () => void}
+   *  Callback function when success
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   */
+  undoDeletePermanent(docId, onSuccess, onError) {
+    let document = {
+      status: STATUS.approved,
+      history: FieldValue.arrayUnion({
+        user: "TODO",
+        event: "TODO: Undid deleting",
+        timestamp: Timestamp.now()
+      })
+    };
+    
+    this.permanentRef.doc(docId).update(document)
+      .then(() => {
+        if (onSuccess) {
+          onSuccess();
+        }
+      })
+      .catch((error) => {
+        if (onError) {
+          onError(error);
+        }
+      });
+  }
+  
+  /**
+   * Approve a document in pending collection.
+   * @param {docId: string}
+   *  Document id
+   * @param {onSuccess?: () => void}
+   *  Callback function when success
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   */
+  approvePending(docId, onSuccess, onError) {
+    let oldDocRef = this.pendingRef.doc(docId);
+    let newDocRef = this.permanentRef.doc(); // put docId in to keep same ID
+    let updateFunction = (transaction) => {
+      return transaction.get(oldDocRef).then((docSnap) => {
+        let doc = docSnap.data();
+        if (!doc) {
+          throw new Error("Document not exist");
+        }
+        
+        doc.status = STATUS.approved;
+        doc.history = FieldValue.arrayUnion({
+          user: "TODO",
+          event: "TODO: Approved",
+          timestamp: Timestamp.now()
+        })
+        
+        transaction.set(newDocRef, doc);
+        transaction.delete(oldDocRef);
+      });
+    };
+    
+    return this.db.runTransaction(updateFunction)
+      .then(() => {
+        if (onSuccess) {
+          onSuccess();
+        }
+      })
+      .catch((error) => {
+        if (onError) {
+          onError(error);
+        }
+      });
+  }
+  
+  /**
+   * Get all participant documents from pending collection.
+   * @param {onChildNext: (doc: Document, newIndex: number,
+   *                       oldIndex: number, type: string) => void}
+   *  Callback function when document changes in the collection
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   * @returns {() => void}
+   *  Unsubscribe function
+   */
+  getPendingList(onChildNext, onError) {
+    return this.pendingRef
+      .onSnapshot({
+        next: (querySnap) => {
+          querySnap.docChanges().forEach((docChg) => {
+            let doc = docChg.doc.data();
+            onChildNext(doc, docChg.newIndex, docChg.oldIndex, docChg.type);
+          });
+        },
+        error: (error) => {
+          if (onError) {
+            onError(error);
+          }
+        }
+      });
+  }
+  
+  /**
+   * Get all approved participant documents from permanent collection.
+   * @param {onChildNext: (doc: Document, newIndex: number,
+   *                       oldIndex: number, type: string) => void}
+   *  Callback function when document changes in the collection
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   * @returns {() => void}
+   *  Unsubscribe function
+   */
+  getApprovedList(onChildNext, onError) {
+    return this.permanentRef.where("status", "==", STATUS.approved)
+      .onSnapshot({
+        next: (querySnap) => {
+          querySnap.docChanges().forEach((docChg) => {
+            let doc = docChg.doc.data();
+            onChildNext(doc, docChg.newIndex, docChg.oldIndex, docChg.type);
+          });
+        },
+        error: (error) => {
+          if (onError) {
+            onError(error);
+          }
+        }
+      });
+  }
+  
+  /**
+   * Get all deleted participant documents from permanent collection.
+   * @param {onChildNext: (doc: Document, newIndex: number,
+   *                       oldIndex: number, type: string) => void}
+   *  Callback function when document changes in the collection
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   * @returns {() => void}
+   *  Unsubscribe function
+   */
+  getDeletedList(onChildNext, onError) {
+    return this.permanentRef.where("status", "==", STATUS.deleted)
+      .onSnapshot({
+        next: (querySnap) => {
+          querySnap.docChanges().forEach((docChg) => {
+            let doc = docChg.doc.data();
+            onChildNext(doc, docChg.newIndex, docChg.oldIndex, docChg.type);
+          });
+        },
+        error: (error) => {
+          if (onError) {
+            onError(error);
+          }
+        }
+      });
+  }
+  
+  /**
+   * Get all participant documents from both pending and permanent collections.
+   * @param {onChildNext: (doc: Document, newIndex: number,
+   *                       oldIndex: number, type: string) => void}
+   *  Callback function when document changes in the collection
+   * @param {onError?: (error: Error) => void}
+   *  Callback function when fail
+   * @returns {() => void}
+   *  Unsubscribe function
+   */
+  getAllDocuments(onChildNext, onError) {
+    let unsubPending = this.getPendingList(onChildNext, onError);
+    let unsubApproved = this.getApprovedList(onChildNext, onError);
+    let unsubDeleted = this.getDeletedList(onChildNext, onError);
+    return () => {
+      unsubPending();
+      unsubApproved();
+      unsubDeleted();
+    };
   }
 }
