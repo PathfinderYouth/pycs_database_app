85305efc9abbef6d464cfcb08e2ab262bb4607a2
Emily DeLisle <emily.delisle@gmail.com>
2020-05-01 11:54:19 -0700

    [FE][Intake form UI] Form step pages (#7)
    * Enabled flipping between form pages
    
    Start on form steps
    
    Added age calculation for form
    
    Basics form fields laid out
    
    Medical info form fields laid out
    
    Finished form step sections
    
    Separated the form steps to their own files to cut down on amount of lines in one file.
    
    Restyled the start page a bit
    
    * Removed commented lines

--- src/intake-form/components/IntakeForm.js
+++ src/intake-form/components/IntakeForm.js
@@ -1,32 +1,67 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { FormStepStart } from './FormStepStart';
+import { FormStepBasics } from './FormStepBasics';
+import { FormStepMedical } from './FormStepMedical';
+import { FormStepCurrentStatus } from './FormStepCurrentStatus';
+import { FormStepQuestions } from './FormStepQuestions';
 import './IntakeForm.css';
-import Logo from '../../assets/Pathfinder-Logo.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: '25ch',
+      minWidth: '25ch',
     },
   },
 }));
 
 export const IntakeForm = (props) => {
+  /* 
+  1 - Start page
+  2 - Basic info
+  3 - Medical info
+  4 - Current status
+  5 - Questions
+  6 - Confirmation page
+  */
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const handleClickBack = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const handleClickNext = () => {
+    if (currentStep < 6) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const getFormStep = (form, step) => {
+    switch (step) {
+      case 2:
+        return <FormStepBasics form={form} />;
+      case 3:
+        return <FormStepMedical form={form} />;
+      case 4:
+        return <FormStepCurrentStatus form={form} />;
+      case 5:
+        return <FormStepQuestions form={form} />;
+      case 6:
+        return <p>Confirmation</p>;
+      default:
+        return <FormStepStart form={form} />;
+    }
+  };
+
   const classes = useStyles();
-  const {
-    values,
-    // errors,
-    // touched,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    isSubmitting,
-  } = props.form;
+  const { form } = props;
+  const { handleSubmit, isSubmitting } = form;
   return (
     <div className={`${classes.root} container`}>
       <div>
@@ -37,52 +72,40 @@ export const IntakeForm = (props) => {
         </Link>
       </div>
       <div className="formContainer">
-        <div className="form">
-          <img
-            src={Logo}
-            alt="Pathfinder Youth Centre Society logo"
-          />
-          <Typography gutterBottom variant="h4">
-            Self-Enrollment System
-          </Typography>
-          <Typography gutterBottom>
-            To begin, please enter your name:
-          </Typography>
-          <Typography gutterBottom variant="body2">
-            * indicates a required field
-          </Typography>
-          <div className="row">
-            <TextField
-              name="nameGiven"
-              label="Given name(s)"
-              value={values.nameGiven}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              variant="outlined"
-              required
-            />
-            <TextField
-              name="nameLast"
-              label="Last name"
-              value={values.nameLast}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              variant="outlined"
-              required
-            />
-          </div>
-        </div>
+        <div className="form">{getFormStep(form, currentStep)}</div>
       </div>
       <div className="bottomBar">
-        <div className="buttonRight">
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleSubmit}
-            disabled={isSubmitting}
-          >
-            Next
-          </Button>
+        <div className="buttonBack">
+          {currentStep !== 1 && (
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleClickBack}
+            >
+              Back
+            </Button>
+          )}
+        </div>
+        <div className="buttonNext">
+          {currentStep < 5 && (
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleClickNext}
+            >
+              Next
+            </Button>
+          )}
+          {currentStep === 5 && (
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+            >
+              Submit
+            </Button>
+          )}
         </div>
       </div>
     </div>
