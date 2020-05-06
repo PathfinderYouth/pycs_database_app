3f3af3a0ec298e713dcf28516ed7bf39198f4238
ciarasouthgate <ciarajsouthgate@gmail.com>
2020-05-05 20:55:41 -0700

    Suggested changes from code review

--- src/database/components/RecordListContainer.js
+++ src/database/components/RecordListContainer.js
@@ -8,64 +8,25 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { TablePagination } from '@material-ui/core';
-import { SortingTableHead } from './SortingTableHead'
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+import { SortingTableHead } from './SortingTableHead';
+import { stableSort, getComparator } from './sortingHelpers';
 
 const headers = [
   { id: 'lastName', disablePadding: true, label: 'Last Name' },
   { id: 'firstName', disablePadding: true, label: 'First Name' },
   { id: 'address', disablePadding: true, label: 'Address' },
-  { id: 'city', disablePadding: true, label: 'City' }
+  { id: 'city', disablePadding: true, label: 'City' },
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   paper: {
     width: '100%',
     marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
+  }
 }));
 
 export const RecordListContainer = (props) => {
@@ -98,10 +59,7 @@ export const RecordListContainer = (props) => {
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <TableContainer>
-          <Table
-            className={classes.table}
-            size='medium'
-          >
+          <Table className={classes.table} size="medium">
             <SortingTableHead
               classes={classes}
               order={order}
@@ -114,17 +72,26 @@ export const RecordListContainer = (props) => {
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((record, index) => {
+                  const { id, lastName, firstName, address, city } = record;
                   return (
                     <TableRow
                       hover
-                      onClick={(event) => handleClick(event, record.id)}
+                      onClick={(event) => handleClick(event, id)}
                       tabIndex={-1}
-                      key={record.id}
+                      key={id}
                     >
-                      <TableCell><Typography>{record.lastName}</Typography></TableCell>
-                      <TableCell><Typography>{record.firstName}</Typography></TableCell>
-                      <TableCell><Typography>{record.address}</Typography></TableCell>
-                      <TableCell><Typography>{record.city}</Typography></TableCell>
+                      <TableCell>
+                        <Typography>{lastName}</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography>{firstName}</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography>{address}</Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography>{city}</Typography>
+                      </TableCell>
                     </TableRow>
                   );
                 })}
@@ -143,5 +110,4 @@ export const RecordListContainer = (props) => {
       </Paper>
     </div>
   );
-
 };
--- src/database/components/SortingTableHead.js
+++ src/database/components/SortingTableHead.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
+import './style/SortingTableHead.css';
 
 export const SortingTableHead = (props) => {
-  const { classes, order, orderBy, onRequestSort, headerCells } = props;
+  const { order, orderBy, onRequestSort, headerCells } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
@@ -14,35 +14,32 @@ export const SortingTableHead = (props) => {
   return (
     <TableHead>
       <TableRow>
-        {headerCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align='left'
-            padding='default'
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
+        {headerCells.map((headCell) => {
+          const { id, label } = headCell;
+          let isOrderBy = orderBy === id;
+          return (
+            <TableCell
+              key={id}
+              align="left"
+              padding="default"
+              sortDirection={isOrderBy ? order : false}
             >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
+              <TableSortLabel
+                active={isOrderBy}
+                direction={isOrderBy ? order : 'asc'}
+                onClick={createSortHandler(id)}
+              >
+                {label}
+                {isOrderBy && (
+                  <span className="visuallyHidden">
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                )}
+              </TableSortLabel>
+            </TableCell>
+          );
+        })}
       </TableRow>
     </TableHead>
   );
-};
-
-SortingTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
 };
