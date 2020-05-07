8cfa8ed560f6c277e469fe05dbc17f7040bc9024
EmilyDeLisle <emily.delisle@gmail.com>
2020-05-07 16:51:24 -0700

    Added breadcrumbs to page Made it so breadcrumbs can allow the user to visit previous steps Added breakpoints to breadcrumbs so it only shows two when on smaller screens

--- src/intake-form/components/FormStepStart.js
+++ src/intake-form/components/FormStepStart.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import Logo from '../../assets/Pathfinder-Logo.jpg';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { FormFieldBuilder } from './FormFieldBuilder';
 import './FormSteps.css';
 
-export const FormStepStart = ({ form, step }) => {
-  const { fields } = step
+export const FormStepStart = () => {
   return (
     <div className="startPageContainer">
       <div className="startPageContents">
-        <img className="logo" src={Logo} alt="Pathfinder Youth Centre Society logo" />
+        <img
+          className="logo"
+          src={Logo}
+          alt="Pathfinder Youth Centre Society logo"
+        />
         <Typography gutterBottom align="center" variant="h4">
           Self-Enrollment System
         </Typography>
-        <Typography gutterBottom align="center" color="textSecondary">
-          To begin, please enter your name:
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="body2"
-          color="textSecondary"
-        >
-          * indicates a required field
-        </Typography>
-        <Grid container spacing={2}>
-          {fields.map((field) => <FormFieldBuilder key={field.name} form={form} field={field} />)}
-        </Grid>
         <div className="formTextContainer">
           <Typography
             gutterBottom
@@ -51,6 +39,9 @@ export const FormStepStart = ({ form, step }) => {
             obligations.
           </Typography>
         </div>
+        <Typography variant="h6" align="center" color="textSecondary">
+          To begin, please click the Next button.
+        </Typography>
       </div>
     </div>
   );
--- src/intake-form/components/IntakeForm.js
+++ src/intake-form/components/IntakeForm.js
@@ -3,33 +3,30 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 import { FormStepStart } from './FormStepStart';
 import { formSteps, requiredFields } from './fields';
 import { FormStepConfirmation } from './FormStepConfirmation';
 import { FormStep } from './FormStep';
 import './IntakeForm.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      minWidth: '25ch',
-    },
-  },
-}));
-
 export const IntakeForm = (props) => {
-  const classes = useStyles();
   const { form } = props;
   const { handleSubmit, isSubmitting } = form;
   const [currentStep, setCurrentStep] = useState(0);
   const recaptchaRef = React.createRef();
   const lastStepNumber = formSteps.length;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up('md'));
+  let visitedSteps = [];
 
   // Validates form on initial load, generating errors that must be cleared in order to proceed
   useEffect(() => {
     props.form.validateForm();
+    visitStep(currentStep);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -57,23 +54,39 @@ export const IntakeForm = (props) => {
   };
 
   /**
+   * Checks current step for errors
+   */
+  const stepHasErrors = () => {
+    const { errors, setFieldTouched } = form;
+    let hasErrors = false;
+    requiredFields[currentStep].forEach((field) => {
+      setFieldTouched(field);
+      if (!!errors[field]) {
+        hasErrors = true;
+      }
+    });
+    return hasErrors;
+  };
+
+  /** 
+   * Adds current step to list of visited steps
+   * @param stepNumber int
+   */
+  const visitStep = (stepNumber) => {
+    if (!visitedSteps.includes(stepNumber)) {
+      visitedSteps.push(stepNumber);
+    }
+  };
+
+  /**
    * Checks if the current form step has any errors and prevents progression if so. If not, proceeds to the next step.
    * @param form Formik form object
    */
   const handleClickNext = (form) => {
-    const { errors, setFieldTouched } = form;
-    let stepHasErrors = false;
-
-    requiredFields[currentStep].forEach((field) => {
-      setFieldTouched(field);
-      if (!!errors[field]) {
-        stepHasErrors = true;
-      }
-    });
-    if (!stepHasErrors) {
+    if (!stepHasErrors()) {
       if (currentStep < lastStepNumber) {
         if (currentStep === lastStepNumber - 1) {
-          recaptchaRef.current.execute()
+          recaptchaRef.current.execute();
         } else {
           toNextPage();
         }
@@ -81,37 +94,100 @@ export const IntakeForm = (props) => {
     }
   };
 
+  /**
+   * Navigates directly to a form step if:
+   * - the step is before the current step
+   * - the current step has no errors and the clicked step is only 1 step ahead
+   * - the current step has no errors and the clicked step has been visited before
+   * @param event click event
+   * @param stepNumber int
+   */
+  const handleClickStep = (event, stepNumber) => {
+    event.preventDefault();
+    if (stepNumber < currentStep) {
+      setCurrentStep(stepNumber);
+    } else if (
+      (stepNumber === currentStep + 1 ||
+        visitedSteps.includes(stepNumber)) &&
+      !stepHasErrors()
+    ) {
+      setCurrentStep(stepNumber);
+    }
+  };
+
+  /**
+   * Gets the component for the current step index
+   * @param form Formik form
+   * @param step int
+   */
   const getFormStep = (form, step) => {
     const currentStep = formSteps[step];
     if (step > 0 && step < lastStepNumber) {
-      return <FormStep form={form} step={currentStep}/>;
+      return <FormStep form={form} step={currentStep} />;
     } else if (step >= lastStepNumber) {
-      return <FormStepConfirmation />
+      return <FormStepConfirmation />;
     } else {
-      return <FormStepStart form={form} step={currentStep}/>
+      return <FormStepStart />;
     }
   };
 
   return (
-    <div className={`${classes.root} container`}>
+    <div className="container">
       <ReCAPTCHA
         ref={recaptchaRef}
         sitekey="6LfukvMUAAAAAGkE5uDvYCqdi-DEKey3J8AiZl8v"
         size="invisible"
         onChange={onCaptchaChanged}
       />
-      <div className="topLeftLink">
-        <Link href="https://pathfinderyouthsociety.org/">
-          <Typography>
+      <div className="form-topBar">
+        <Typography display="inline">
+          <Link href="https://pathfinderyouthsociety.org/">
             &lt; Back to pathfinderyouthsociety.org
-          </Typography>
-        </Link>
+          </Link>
+        </Typography>
       </div>
-      <div className="formContainer">
+
+      {currentStep !== 0 && currentStep !== lastStepNumber && (
+        <div className="form-breadcrumbs">
+          <Breadcrumbs
+            maxItems={isMobile ? undefined : 2}
+            separator={<NavigateNextIcon fontSize="small" />}
+            aria-label="breadcrumb"
+          >
+            {formSteps.map(
+              (step, index) =>
+                index > 0 &&
+                (currentStep === index ? (
+                  <Typography
+                    key={step.stepName}
+                    variant="caption"
+                    color="textSecondary"
+                  >
+                    {step.stepName}
+                  </Typography>
+                ) : (
+                  <Link
+                    key={step.stepName}
+                    variant="caption"
+                    href="#"
+                    onClick={(event) => handleClickStep(event, index)}
+                  >
+                    {step.stepName}
+                  </Link>
+                )),
+            )}
+            <Typography variant="caption" color="textPrimary">
+              Confirmation
+            </Typography>
+          </Breadcrumbs>
+        </div>
+      )}
+
+      <div className="form-container">
         <div className="form">{getFormStep(form, currentStep)}</div>
       </div>
-      <div className="bottomBar">
-        <div className="buttonBack">
+      <div className="form-bottomBar">
+        <div className="form-buttonBack">
           {currentStep !== 0 && (
             <Button
               color="primary"
@@ -149,23 +225,23 @@ export const IntakeForm = (props) => {
               Next
             </Button>
           )}
-            {currentStep === lastStepNumber - 1 && (
-              <Button
-                color="primary"
-                variant="contained"
-                onClick={() => {
-                  handleSubmit(form.values, form);
-                  // TODO: only proceed to next step if pushing to database was successful,
-                  // otherwise, show an error message somehow
-                  handleClickNext(form);
-                }}
-                disabled={isSubmitting}
-              >
-                Submit
-              </Button>
-            )}
-          </div>
+          {currentStep === lastStepNumber - 1 && (
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => {
+                handleSubmit(form.values, form);
+                // TODO: only proceed to next step if pushing to database was successful,
+                // otherwise, show an error message somehow
+                handleClickNext(form);
+              }}
+              disabled={isSubmitting}
+            >
+              Submit
+            </Button>
+          )}
         </div>
+      </div>
     </div>
   );
 };
