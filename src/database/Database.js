65b24e02aacdd5b2d7fa6136ac15edc562893a33
EmilyDeLisle <emily.delisle@gmail.com>
2020-05-11 14:10:24 -0700

    Removed state from Database.js and sub components and put it in UIStore Added `setCurrentParticipant` to ParticipantStore Added some constants to ParticipantStore Cleaned up some code/warnings

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   DetailViewDrawer,
   ListContainer,
   ListViewDrawer,
   NavDrawer,
-  ParticipantTabs,
   TopNavBar,
-  viewModes,
 } from './components';
 import { makeStyles } from '@material-ui/core/styles';
 import { inject, observer } from 'mobx-react';
-import { participantStore } from '../injectables';
+import { participantStore, uiStore } from '../injectables';
 import './Database.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,143 +18,98 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 // container that holds all database UI objects
-export const Database = inject('participantStore')(
+export const Database = inject('participantStore', 'uiStore')(
   observer(() => {
     const classes = useStyles();
-    const participantHeaders = [
-      { id: 'status', label: 'Status' },
-      { id: 'lastName', label: 'Last Name' },
-      { id: 'firstName', label: 'First Name' },
-      { id: 'birthDate', label: 'Date of Birth' },
-      { id: 'address', label: 'Address' },
-      { id: 'city', label: 'City' },
-    ];
-    const staffHeaders = [
-      { id: 'email', label: 'Email' },
-      { id: 'name', label: 'Name' },
-      { id: 'role', label: 'Role' },
-    ];
+    const {
+      viewModes,
+      currentViewMode,
+      navigationDrawerOpen,
+      setNavigationDrawerOpen,
+    } = uiStore;
+    const {
+      participants,
+      numOfNewParticipants,
+      setCurrentParticipant,
+    } = participantStore;
 
-    const [drawerState, setDrawerState] = useState(false);
-    const [viewMode, setViewMode] = useState(viewModes.PARTICIPANT_LIST);
-
-    // Get record clicked from RecordListContainer.js and passed to RecordDialog.js
-    const [openParticipantRecord, setOpenParticipantRecord] = useState(null);
-    const [openStaffRecord, setOpenStaffRecord] = useState(null);
-    const { participants, setCollection, numOfNewParticipants } = participantStore;
-
-    const handleDrawerOpen = () => {
-      setDrawerState(true);
-    };
-
-    const handleDrawerClose = () => {
-      setDrawerState(false);
-    };
-
-    const handleParticipantOpen = () => {
-      setViewMode(viewModes.PARTICIPANT_DETAIL);
-    };
-
-    const handleParticipantClose = () => {
-      setViewMode(viewModes.PARTICIPANT_LIST);
-    };
-
-    const handleStaffOpen = () => {
-      setViewMode(viewModes.STAFF_DETAIL);
-    };
-
-    const handleStaffClose = () => {
-      setViewMode(viewModes.STAFF_LIST);
-    };
-
-    const handleViewDrawerClick = (itemClicked) => {
-      //TODO change modes/make queries depending on what was clicked
-    };
-
-    const listViewDrawer = (
-      <ListViewDrawer
-        handleClick={handleViewDrawerClick}
-        numNew={numOfNewParticipants}
-        classes={classes}
-      />
-    );
-
-    const participantListView = (
-      <ListContainer
-        headers={participantHeaders}
-        records={participants}
-        setRowClicked={setOpenParticipantRecord}
-        handleRowOpened={handleParticipantOpen}
-        handleRowClosed={handleParticipantClose}
-      />
-    );
-
-    const [navButtons, setNavButtons] = useState(listViewDrawer);
-    const [contentView, setContentView] = useState(participantListView);
-
-    const handleChangeParticipantTab = () => {
-      //TODO
-    };
-
-    const switchNavDrawer = () => {
-      switch (viewMode) {
+    /**
+     * Gets content of side drawer
+     */
+    const getNavDrawerContents = () => {
+      switch (currentViewMode) {
+        // detail modes
+        case viewModes.PARTICIPANT_DETAIL:
+        case viewModes.STAFF_DETAIL:
+          return (
+            <DetailViewDrawer/>
+          );
+        // list modes
         case viewModes.PARTICIPANT_LIST:
         case viewModes.STAFF_LIST:
-          return listViewDrawer;
-        case viewModes.PARTICIPANT_DETAIL:
+        default:
           return (
-            <DetailViewDrawer
-              handleClickBack={handleParticipantClose}
-              subComponent={<ParticipantTabs handleClick={handleChangeParticipantTab()} />}
+            <ListViewDrawer
+              numNew={numOfNewParticipants}
+              classes={classes}
             />
           );
-        case viewModes.STAFF_DETAIL:
-          return <DetailViewDrawer handleClickBack={handleStaffClose()} />;
-        default:
-          return null;
       }
     };
 
-    const switchContentView = () => {
-      switch (viewMode) {
-        case viewModes.PARTICIPANT_LIST:
-          return participantListView;
-        case viewModes.PARTICIPANT_DETAIL:
-          return null; //TODO replace with participant details
-        case viewModes.STAFF_LIST:
-          return (
-            <ListContainer
-              headers={staffHeaders}
-              records={null} //TODO
-              setRowClicked={setOpenStaffRecord}
-              handleRowOpened={handleStaffOpen}
-              handleRowClosed={handleStaffClose}
-            />
-          );
-        case viewModes.STAFF_DETAIL:
-          return null; //TODO replace with staff details
-        default:
-          return null;
-      }
+    /**
+     * Gets content of list view
+     */
+    const getListView = () => {
+      const listViewProps =
+        currentViewMode === viewModes.STAFF_LIST
+          ? {
+              records: [], // TODO: get user list from user store
+              // TODO: set current user in user store
+              setRowClicked: () => console.log('Opening staff record'), 
+            }
+          : {
+              records: participants,
+              setRowClicked: setCurrentParticipant,
+            };
+
+      return <ListContainer {...listViewProps} />;
     };
 
-    useEffect(() => {
-      // Do this after the component is initialized
-      setCollection('new');
-      switchContentView();
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    /**
+     * Gets content of main page view
+     */
+    const getContent = () => {
+      switch (currentViewMode) {
+        case viewModes.STAFF_DETAIL:
+          return <div>Staff Detail</div>; //TODO replace with staff detail page
+        
+        case viewModes.PARTICIPANT_DETAIL:
+          return <div>Participant Detail</div>; //TODO replace with participant detail page
+      
+        case viewModes.STATISTICS:
+          return <div>Statistics</div>; //TODO replace with statistics page
+        
+        case viewModes.STAFF_LIST:
+        case viewModes.PARTICIPANT_LIST:
+        default:
+          return getListView();
+      }
+    };
 
     return (
       <div className={`${classes.root} root`}>
-        <TopNavBar handleDrawerOpen={handleDrawerOpen} handleDrawerState={drawerState} />
-        <NavDrawer
-          handleDrawerClose={handleDrawerClose}
-          handleDrawerState={drawerState}
-          subComponent={switchNavDrawer()}
+        <TopNavBar
+          handleDrawerOpen={() => setNavigationDrawerOpen(true)}
+          drawerState={navigationDrawerOpen}
         />
-        <div className={`${classes.content} content`}>{switchContentView()}</div>
+        <NavDrawer
+          handleDrawerClose={() => setNavigationDrawerOpen(false)}
+          drawerState={navigationDrawerOpen}
+        >
+          {getNavDrawerContents()}
+        </NavDrawer>
+        <div className={`${classes.content} content`}>{getContent()}</div>
       </div>
     );
   }),
