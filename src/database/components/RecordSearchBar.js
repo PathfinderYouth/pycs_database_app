8ed6e572365745b7546a81a2ea2e11ad3ccf78c1
ciarasouthgate <ciarajsouthgate@gmail.com>
2020-05-12 11:10:04 -0700

    Added a button and submit methods

--- src/database/components/RecordSearchBar.js
+++ src/database/components/RecordSearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Search from '@material-ui/icons/Search';
 import TextField from '@material-ui/core/TextField';
@@ -8,17 +8,41 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import './style/RecordSearchBar.css';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: { padding: theme.spacing(2) },
+  button: { marginLeft: theme.spacing(1) },
 }));
 
 export const RecordSearchBar = (props) => {
   const classes = useStyles();
   const { title, headers } = props;
+  const [searchDefault, setSearchDefault] = useState(headers[0].id);
+  const [searchBy, setSearchBy] = useState(searchDefault);
+  const [searchText, setSearchText] = useState('');
 
   const handleChange = (event) => {
-    //TODO use selection for search query
+    setSearchBy(event.target.value);
+  };
+
+  useEffect(() => {
+    setSearchDefault(headers[0].id);
+  }, [headers]);
+
+  const getSearchText = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const onKeyPressed = (event) => {
+    if (event.key === 'Enter') {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    console.log(searchText + ' ' + searchBy);
+    //TODO return this to whatever is doing the search
   };
 
   return (
@@ -28,6 +52,10 @@ export const RecordSearchBar = (props) => {
       </Typography>
       <TextField
         className="searchBox"
+        variant="outlined"
+        size="small"
+        onChange={getSearchText}
+        onKeyPress={onKeyPressed}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
@@ -36,26 +64,31 @@ export const RecordSearchBar = (props) => {
           ),
           endAdornment: (
             <InputAdornment position="end">
-              <FormControl className="formControl">
-                <Select
-                  className="formControl"
-                  defaultValue={headers[0].id}
-                  onChange={handleChange}
-                  disableUnderline
-                >
-                  {headers.map((header) => {
-                    return (
-                      <MenuItem value={header.id} key={header.id}>
-                        {header.label}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
+              <div className="endAdorn">
+                <FormControl className="formControl">
+                  <Select
+                    className="formControl"
+                    defaultValue={headers[0].id}
+                    onChange={handleChange}
+                    disableUnderline
+                  >
+                    {headers.map((header) => {
+                      return (
+                        <MenuItem value={header.id} key={header.id}>
+                          {header.label}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </div>
             </InputAdornment>
           ),
         }}
       />
+      <Button className={classes.button} variant="contained" color="primary" onClick={onSubmit}>
+        Search
+      </Button>
     </div>
   );
 };
