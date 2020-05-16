368d353742cd98e52ec88fb79e6e45129e7759b7
EmilyDeLisle <emily.delisle@gmail.com>
2020-05-16 13:42:31 -0700

    Form submits birthdates in utc format

--- src/intake-form/IntakeFormPage.js
+++ src/intake-form/IntakeFormPage.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { useSnackbar } from 'notistack';
+import moment from 'moment';
 import service from '../facade/service';
 import { IntakeForm, validationSchema } from './components';
 import { initialValues } from '../fields';
 
 export const IntakeFormPage = () => {
   const { enqueueSnackbar } = useSnackbar();
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting }) => {
         service.getDatabase().addNew(
-          values,
+           // formats the birthdate field into UTC
+          { ...values, birthDate: moment(values.birthDate).utc().format() },
           (docId) => {
             setSubmitting(false);
             enqueueSnackbar('Application successfully submitted.', {
