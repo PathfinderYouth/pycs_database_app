47a82f1f77642fc46aff8ef58d08380c0325cab4
bryan7zhang <bryan7zhang@gmail.com>
2020-05-16 05:35:48 -0700

    implemented add new staff functionality

--- src/database/components/ListContainer.js
+++ src/database/components/ListContainer.js
@@ -14,12 +14,23 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 import { RecordSearchBar } from './RecordSearchBar';
 import { SortingTableHead } from './SortingTableHead';
 import { uiStore, participantStore } from '../../injectables';
 import { viewModes, participantDetailViewModes, collectionType } from '../../constants';
 import './style/ListContainer.css';
+import service from '../../facade/service';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -114,6 +125,43 @@ export const ListContainer = inject(
         // TODO handle Delete functionlity of user management
       };
 
+      const [addStaffOpen, setAddStaffOpen] = useState(false);
+      let db = service.getUserList();
+
+      const [email, setEmail] = useState(null);
+      const [name, setName] = useState(null);
+      const [role, setRole] = useState(null);
+
+      const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+      };
+
+      const handleNameChange = (event) => {
+        setName(event.target.value);
+      };
+
+      const handleRoleChange = (event) => {
+        setRole(event.target.value);
+      };
+
+      const handleAddIconClickOpen = () => {
+        setAddStaffOpen(true);
+      };
+
+      const handleAddDialogClose = () => {
+        setAddStaffOpen(false);
+      };
+
+      const handleAddNewUser = () => {
+        if (!email && !name && !role) {
+          alert("New user's email, name, and role must be provided");
+          return;
+        }
+        let newUser = { email: email, name: name, role: role };
+        db.addUser(newUser);
+        setAddStaffOpen(false);
+      };
+
       return (
         <div className={`${classes.root} maxWidth`}>
           <Paper className={`${classes.paper} maxWidth`}>
@@ -132,34 +180,56 @@ export const ListContainer = inject(
                   rowCount={records.length}
                   headerCells={headers}
                 />
-                <TableBody>
-                  {records.map((row) => (
-                    <TableRow hover key={row.id} onClick={() => handleRowClicked(row)}>
-                      {headers.map((column) => (
-                        <TableCell key={`${row.id}-${column.id}`}>
-                          {currentViewMode === viewModes.STAFF_LIST && column.id === 'action' ? (
-                            <>
-                              <Tooltip title="Edit user" aria-label="edit" placement="bottom">
-                                <IconButton color="inherit">
-                                  <EditIcon onClick={() => handleEditIconClicked(row)} />
-                                </IconButton>
-                              </Tooltip>
-                              <Tooltip title="Delete user" aria-label="delete" placement="bottom">
-                                <IconButton color="inherit">
-                                  <DeleteIcon onClick={() => handleDeleteIconClicked(row)} />
-                                </IconButton>
-                              </Tooltip>
-                            </>
-                          ) : (
+                {currentViewMode === viewModes.STAFF_LIST ? (
+                  <TableBody>
+                    {records.map((row) => (
+                      <TableRow hover key={row.id}>
+                        {headers.map((column) => (
+                          <TableCell key={`${row.id}-${column.id}`}>
+                            {column.id === 'action' ? (
+                              <>
+                                <Tooltip title="Edit user" aria-label="edit" placement="bottom">
+                                  <IconButton
+                                    color="inherit"
+                                    onClick={() => handleEditIconClicked(row)}
+                                  >
+                                    <EditIcon />
+                                  </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Delete user" aria-label="delete" placement="bottom">
+                                  <IconButton
+                                    color="inherit"
+                                    onClick={() => handleDeleteIconClicked(row)}
+                                  >
+                                    <DeleteIcon />
+                                  </IconButton>
+                                </Tooltip>
+                              </>
+                            ) : (
+                              <div className="list-row">
+                                <Typography>{row[column.id]}</Typography>
+                              </div>
+                            )}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                ) : (
+                  <TableBody>
+                    {records.map((row) => (
+                      <TableRow hover key={row.id} onClick={() => handleRowClicked(row)}>
+                        {headers.map((column) => (
+                          <TableCell key={`${row.id}-${column.id}`}>
                             <div className="list-row">
                               <Typography>{row[column.id]}</Typography>
                             </div>
-                          )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-                </TableBody>
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                )}
               </Table>
             </TableContainer>
             <TablePagination
@@ -192,14 +262,64 @@ export const ListContainer = inject(
                 onClick={() => {
                   currentViewMode === viewModes.PARTICIPANT_LIST
                     ? handleClickNew()
-                    : // TODO: Add add staff method
-                      console.log('Add new staff account');
+                    : handleAddIconClickOpen();
                 }}
               >
                 <AddIcon />
               </Fab>
             </Tooltip>
           )}
+
+          <Dialog open={addStaffOpen} onClose={setAddStaffOpen} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create a new user</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                required
+                margin="dense"
+                id="email"
+                label="Email"
+                type="email"
+                size="medium"
+                onChange={handleEmailChange}
+                fullWidth
+              />
+              <TextField
+                required
+                margin="dense"
+                id="name"
+                label="Name"
+                type="name"
+                size="medium"
+                onChange={handleNameChange}
+                fullWidth
+              />
+              <FormControl required fullWidth>
+                <InputLabel id="demo-simple-select-required-label">Role</InputLabel>
+                <Select
+                  labelId="demo-simple-select-required-label"
+                  id="demo-simple-select-required"
+                  value={role}
+                  onChange={handleRoleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={'staff'}>Staff</MenuItem>
+                  <MenuItem value={'admin'}>Admin</MenuItem>
+                </Select>
+                {/* <FormHelperText>Required</FormHelperText> */}
+              </FormControl>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleAddNewUser} color="primary">
+                Create
+              </Button>
+              <Button onClick={handleAddDialogClose} color="primary">
+                Cancel
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       );
     },
