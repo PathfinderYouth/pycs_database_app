68a2a3e99287d1107dcb08e65138ccd0142f3202
Emily DeLisle <emily.delisle@gmail.com>
2020-05-18 11:14:41 -0700

    Added error handling for empty notes (#97)
    Bypass pushing an update to the db if a user tries updating a record but makes no changes

--- src/database/components/participantDetail/ParticipantDetailEdit.js
+++ src/database/components/participantDetail/ParticipantDetailEdit.js
@@ -2,20 +2,20 @@ import React, { useContext } from 'react';
 import { useSnackbar } from 'notistack';
 import { participantDetailSteps } from '../../../fields';
 import { AuthContext } from '../../../sign-in';
-import { collectionType, participantDetailViewModes } from '../../../constants';
+import { participantDetailViewModes, status } from '../../../constants';
 import service from '../../../facade/service';
 import { ParticipantDetailForm } from './ParticipantDetailForm';
 import '../style/ParticipantDetailPage.css';
 
 export const ParticipantDetailEdit = ({
   participant,
-  collection,
   currentStep,
   handleClickChangeMode,
   onSuccessfulEdit,
 }) => {
   const { enqueueSnackbar } = useSnackbar();
   const step = participantDetailSteps[currentStep];
+  const { status: participantStatus } = participant;
   const {
     currentUser: { email, displayName },
   } = useContext(AuthContext);
@@ -23,49 +23,55 @@ export const ParticipantDetailEdit = ({
 
   const handleSubmit = (values, setSubmitting) => {
     const db = service.getDatabase();
-    collection === collectionType.NEW
-      ? db.updateNew(
-          participant, // old data
-          values, // new data from form
-          userID, // user
-          (updatedParticipant) => {
-            setSubmitting(false);
-            enqueueSnackbar('Participant record updated.', {
-              variant: 'success',
-            });
-            onSuccessfulEdit(updatedParticipant);
-            handleClickChangeMode();
-          },
-          (error) => {
-            enqueueSnackbar('There was a problem updating the participant record.', {
-              variant: 'error',
-            });
-            handleClickChangeMode();
-          },
-        )
-      : db.updatePermanent(
-          participant,
-          values,
-          userID,
-          (updatedParticipant) => {
-            setSubmitting(false);
-            enqueueSnackbar('Participant record updated.', {
-              variant: 'success',
-            });
-            onSuccessfulEdit(updatedParticipant);
-            handleClickChangeMode();
-          },
-          (error) => {
-            let message = error.name === 'DuplicateError'
-              ? 'Unable to save participant record, record with that SIN already exists'
-              : 'There was a problem saving the participant record.';
+    if (participant !== values) { // if there were changes
+      participantStatus === status.NEW
+        ? db.updateNew(
+            participant, // old data
+            values, // new data from form
+            userID, // user
+            (updatedParticipant) => {
+              setSubmitting(false);
+              enqueueSnackbar('Participant record updated.', {
+                variant: 'success',
+              });
+              onSuccessfulEdit(updatedParticipant);
+              handleClickChangeMode();
+            },
+            (error) => {
+              enqueueSnackbar('There was a problem updating the participant record.', {
+                variant: 'error',
+              });
+              handleClickChangeMode();
+            },
+          )
+        : db.updatePermanent(
+            participant,
+            values,
+            userID,
+            (updatedParticipant) => {
+              setSubmitting(false);
+              enqueueSnackbar('Participant record updated.', {
+                variant: 'success',
+              });
+              onSuccessfulEdit(updatedParticipant);
+              handleClickChangeMode();
+            },
+            (error) => {
+              let message =
+                error.name === 'DuplicateError'
+                  ? 'Unable to save participant record, record with that SIN already exists'
+                  : 'There was a problem saving the participant record.';
 
-            enqueueSnackbar(message, {
-              variant: 'error',
-            });
-            handleClickChangeMode();
-          },
-        );
+              enqueueSnackbar(message, {
+                variant: 'error',
+              });
+              handleClickChangeMode();
+            },
+          );
+    } else {
+      enqueueSnackbar('No changes made.');
+      handleClickChangeMode();
+    }
   };
 
   return (
