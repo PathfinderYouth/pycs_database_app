b3c385e7ec361daaa291758293e7de7b66ec8a0d
bryan7zhang <bryan7zhang@gmail.com>
2020-05-19 16:23:56 -0700

    added firebase session persistence

--- src/facade/AuthenticationManager.js
+++ src/facade/AuthenticationManager.js
@@ -76,9 +76,9 @@ export default class AuthenticationManager {
 
   /**
    * set up firebase authentication sign in session
-   * if the environment support session auth state is persisted in current
-   * session only. Closing the window would clear any existing state
-   * even if a user forgets to sign out
+   * if the environment support session persistence, auth state is persisted 
+   * in current session only. Closing the window would clear any existing state
+   * even if a user forgets to sign out.
    * @param {onSuccess?: () => void}
    *  Callback function when success
    * @param {onError?: (error: Error) => void}
