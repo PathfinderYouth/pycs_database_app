9a6b3acf435510d1ad28a2f78ffcb327bcf53dbc
Lawrance On <54971930+lawranceo1@users.noreply.github.com>
2021-04-18 17:30:43 -0700

    host status implemented, servicejs uses dummy database (#145)
    Co-authored-by: PathfinderYouth <65784065+PathfinderYouth@users.noreply.github.com>

--- src/facade/service.js
+++ src/facade/service.js
@@ -3,15 +3,13 @@ import DatabaseManager from './DatabaseManager';
 import AuthenticationManager from './AuthenticationManager';
 import UserListManager from './UserListManager';
 
-const CONFIG = {
-  apiKey: 'AIzaSyDDYYHzgfZh5XCBLgEaPSHaI1RnBYAanrw',
-  authDomain: 'pycs-database-app.firebaseapp.com',
-  databaseURL: 'https://pycs-database-app.firebaseio.com',
-  projectId: 'pycs-database-app',
-  storageBucket: 'pycs-database-app.appspot.com',
-  messagingSenderId: '431213152640',
-  appId: '1:431213152640:web:8783ba86ce18995a9e5965',
-  measurementId: 'G-MHTVV1X9ZH',
+const testCONFIG = {
+  apiKey: "AIzaSyBYrEGT3ocS4VIdd_YxC3raWQJ_vf2m2IQ",
+  authDomain: "pycs-development-project.firebaseapp.com",
+  projectId: "pycs-development-project",
+  storageBucket: "pycs-development-project.appspot.com",
+  messagingSenderId: "92261316097",
+  appId: "1:92261316097:web:d3d19ee1406312d4f4f200"
 };
 
 const testCONFIG = {
